b4101531b7d6b376a316e31c0559a253028693ab
Payam Hoseini <p.hoseini.ch@gmail.com>
2024-05-26 22:29:53 +0330

    Complete single page ui which is for every post based on post id. For side menu of this page i use some dummy data again.

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import Edit from "../img/edit.png";
+import Delete from "../img/delete.png";
+import { Link } from "react-router-dom";
+import Menu from "../components/Menu";
 
 const Single = () => {
-  return <div>Single</div>;
+  return (
+    <div className="single">
+      <div className="content">
+        <img
+          src="https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+        />
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+          <div className="info">
+            <span>username payam</span>
+            <p>Posted 2 days ago</p>
+          </div>
+
+          <div className="edit">
+            <Link to={`/write?edit=2`}>
+              <img src={Edit} alt="" />
+            </Link>
+            <img src={Delete} alt="" />
+          </div>
+        </div>
+        <h1>Post title</h1>
+        <p>Post Description</p>{" "}
+      </div>
+      <Menu />
+    </div>
+  );
 };
 
 export default Single;
