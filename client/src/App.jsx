24d155baf32117108229c3674739ea87a3395ad1
Payam Hoseini <p.hoseini.ch@gmail.com>
2024-05-26 20:47:01 +0330

    Create Login and Register pages ui.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,8 @@ import Single from "./pages/Single";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
+import "./style.scss";
+
 const Layout = () => {
   return (
     <>
