2e23d4fb8780e4543e66299a933bc9c9dcfde277
Payam Hoseini <p.hoseini.ch@gmail.com>
2024-05-27 20:56:47 +0330

    Install backend dependencies and run express server.

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const Write = () => {
   const [value, setValue] = useState("");
