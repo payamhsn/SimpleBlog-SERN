39fa3ea9fead2e34120d3687a1f8d881eb1f107e
Payam Hoseini <p.hoseini.ch@gmail.com>
2024-05-29 20:29:10 +0330

    Complete login functionality by creating a token and store it in browser.We can use this token to check a post writer by id in db and let them to edit or delete.

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,6 @@
 import { db } from "../db.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = (req, res) => {
   //CHECK EXISTING USER
@@ -24,6 +25,34 @@ export const register = (req, res) => {
   // console.log(req.body);
 };
 
-export const login = (req, res) => {};
+export const login = (req, res) => {
+  //CHECK USER
+  const q = "SELECT * FROM users WHERE username = ?";
+
+  db.query(q, [req.body.username], (err, data) => {
+    // console.log(data);
+    if (err) return res.status(500).json(err);
+    if (data.length === 0) return res.status(404).json("User not found!");
+
+    //Check password
+    const isPasswordCorrect = bcrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
+
+    if (!isPasswordCorrect)
+      return res.status(400).json("Wrong username or password!");
+
+    const token = jwt.sign({ id: data[0].id }, process.env.JWT_KEY);
+    const { password, ...other } = data[0];
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(other);
+  });
+};
 
 export const logout = (req, res) => {};
--- api/index.js
+++ api/index.js
@@ -4,9 +4,12 @@ import postRoutes from "./routes/posts.js";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 
+import cookieParser from "cookie-parser";
+
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/posts", postRoutes);
