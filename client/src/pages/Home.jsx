b156dda8ecfd60e69596d53eee1e6b981af9cb2d
Payam Hoseini <p.hoseini.ch@gmail.com>
2024-05-30 17:01:40 +0330

    With help of useLocation, i get category query from url and send it to db to get all the posts based on categories.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import axios from "axios";
 
 const Home = () => {
@@ -34,17 +34,19 @@ const Home = () => {
 
   const [posts, setPosts] = useState([]);
 
+  const cat = useLocation().search;
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get(`/api/posts`);
+        const res = await axios.get(`/api/posts${cat}`);
         setPosts(res.data);
       } catch (err) {
         console.log(err);
       }
     };
     fetchData();
-  }, []);
+  }, [cat]);
 
   return (
     <div className="home">
