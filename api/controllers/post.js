19d9c7ac73af0ba600fe6784ca0a113abb166042
Payam Hoseini <p.hoseini.ch@gmail.com>
2024-05-30 16:55:54 +0330

    Complete posts controller for get all the posts from db and show them in home page.

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,5 +1,15 @@
+import { db } from "../db.js";
+
 export const getPosts = (req, res) => {
-  return res.json("from controller");
+  const q = req.query.cat
+    ? "SELECT * FROM posts WHERE cat=?"
+    : "SELECT * FROM posts";
+
+  db.query(q, [req.query.cat], (err, data) => {
+    if (err) return res.status(500).send(err);
+
+    return res.status(200).json(data);
+  });
 };
 
 export const getPost = (req, res) => {
