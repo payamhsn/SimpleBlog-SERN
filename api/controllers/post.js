e558016b92977a033151ee925242f401618aa04a
Payam Hoseini <p.hoseini.ch@gmail.com>
2024-05-30 16:30:58 +0330

    Begin posts functionality with creating routes structures and controllers.

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,3 +1,19 @@
+export const getPosts = (req, res) => {
+  return res.json("from controller");
+};
+
+export const getPost = (req, res) => {
+  return res.json("from controller");
+};
+
 export const addPost = (req, res) => {
+  return res.json("from controller");
+};
+
+export const deletePost = (req, res) => {
+  return res.json("from controller");
+};
+
+export const updatePost = (req, res) => {
   return res.json("from controller");
 };
